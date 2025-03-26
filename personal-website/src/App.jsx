10cf2c6989fbf4d634f53ab1f13b94e6516eb5cd
William Yang <willi64645@gmail.com>
2025-03-26 11:59:29 -0400

    a350 now fill sth4e entire screen

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -11,39 +11,45 @@ function AirbusModel() {
       ref={modelRef}
       object={scene}
       position={[0, 0, 0]}
-      scale={5} // Increased scale significantly
+      scale={5}
     />
   );
 }
 
 export default function App() {
   return (
-    <Canvas
-      className="w-full h-screen"
-      camera={{
-        position: [0, 20, 50], // Centered and closer
-        fov: 50,
-        near: 0.1,
-        far: 10000
-      }}
-    >
-      <ambientLight intensity={0.7} />
-      <spotLight
-        position={[100, 100, 100]}
-        intensity={1.5}
-        angle={0.3}
-        penumbra={1}
-      />
-      <directionalLight
-        position={[-100, 100, -100]}
-        intensity={0.5}
-      />
-      <AirbusModel />
-      <OrbitControls
-        enablePan={true}
-        enableZoom={true}
-        target={[0, 0, 0]} // Centered target
-      />
-    </Canvas>
+    <div className="fixed inset-0 overflow-hidden">
+      <h1 className="absolute z-10 text-white text-4xl top-10 left-10">
+        This is a test text!
+      </h1>
+
+      <Canvas
+        className="w-full h-full"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+        }}
+        camera={{
+          position: [0, 1000, 2000],
+          fov: 60,
+          near: 0.1,
+          far: 50000,
+        }}
+      >
+        <ambientLight intensity={0.7} />
+        <spotLight
+          position={[100, 100, 100]}
+          intensity={1.5}
+          angle={0.3}
+          penumbra={1}
+        />
+        <directionalLight position={[-100, 100, -100]} intensity={0.5} />
+        <AirbusModel />
+        <OrbitControls enablePan={true} enableZoom={true} target={[0, 0, 0]} />
+      </Canvas>
+    </div>
   );
 }
