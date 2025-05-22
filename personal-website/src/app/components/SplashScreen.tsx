a0f7f6bb8d019cc50fbd0556152395b10c432187
William Yang <willi64645@gmail.com>
2025-05-22 14:25:51 -0400

    feat: main custom pages, font, splash screen

---
+++ personal-website/src/app/components/SplashScreen.tsx
@@ -0,0 +1,9 @@
+export default function SplashScreen() {
+    return (
+        <div className="h-screen w-screen bg-black text-green-400 flex items-center justify-center text-center">
+            <h1 className="text-xl md:text-2xl font-mono">
+                LOADING PORTFOLIO<br />(MAX 3 SECONDS)
+            </h1>
+        </div>
+    );
+}
