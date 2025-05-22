bab292e8035dafe9b97502bbe8eee3ca39b89eef
William Yang <willi64645@gmail.com>
2025-05-22 15:13:01 -0400

    feat: init commit (v2)

--- personal-website/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-      <div className="flex items-center justify-center min-h-screen font-[MFDFont]">
-        <h1 className="text-3xl">HELLO WRORLD</h1>
-      </div>
-  );
-}
